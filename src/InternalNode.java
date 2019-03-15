997dfb2d8323b584b7d0f325799d0e48fc8f162a
tsinghqs <tsingh2324@gmail.com>
2019-03-14 21:47:16 -0400

    Added Nodes

--- src/InternalNode.java
+++ src/InternalNode.java
@@ -1,24 +1,66 @@
 
+/*
+ - InternaLeafNode: Models intermediate nodes.
+ - Contains exactly 5 children
+ - Must contain absolutely NO OTHER data.
+*/
 public class InternalNode extends DNANode {
+    private static final int NCHILDREN = 5;
+    private final DNANode[] children = new DNANode[NCHILDREN];
     
-    private DNANode aNode;
-    private DNANode cNode;
-    private DNANode gNode;
-    private DNANode tNode;
-    private DNANode dollarNode;
-    
-    public InternalNode(DNANode flyweightNode)
-    {
-        aNode = flyweightNode;
-        cNode = flyweightNode;
-        gNode = flyweightNode;
-        tNode = flyweightNode;
-        dollarNode = flyweightNode;
+    // Creates an instance with all Empty child nodes
+    public InternalNode() {
+        for (int i = 0; i < children.length; i++) {
+            children[i] = EmptyNode.getInst();
+        }
     }
     
-    public void insert(char[] seq)
-    {
+    public void printInfo(int level, int type) {
         
+        // Print "I"
+        System.out.println("I");
+        
+        // And, print all children at level+1 indent
+        for (DNANode child : children) {
+            child.print(level + 1, type);
+        }
     }
-
+    
+    public void insert(int level, LeafNode newnode) {
+        
+        System.out.println("InternaNodeInsert: " + level + " " + newnode);
+        char ch = newnode.getCharAt(level);
+        int position = 0;
+        switch (ch) {
+            case 'A': position = 0; break;
+            case 'C': position = 1; break;
+            case 'G': position = 2; break;
+            case 'T': position = 3; break;
+            case 0: position = 4; break;
+        }
+        DNANode child = children[position];
+        if (child instanceof EmptyNode) {
+            System.out.println("    Replacing EmptyNode @ " + position);
+            children[position] = newnode;
+            return;
+        }
+        if (child instanceof LeafNode) {
+            if (position == 4) {
+                System.out.println("Sequence already exists! (in $-bucket)");
+                return;
+            }
+            
+            System.out.println("    Replacing LeafNode @ " + position);
+            InternalNode InternaLeafNode = new InternalNode();
+            children[position] = InternaLeafNode;
+            LeafNode LeafNode = (LeafNode) child;
+            InternaLeafNode.insert(level + 1, LeafNode);
+            InternaLeafNode.insert(level + 1, newnode);
+            return;
+        }
+        
+        System.out.println("    Processing InternalNode @ " + position);
+        InternalNode InternaLeafNode = (InternalNode) child;
+        InternaLeafNode.insert(level + 1, newnode);
+    }
 }
--- src/LeafNode.java
+++ src/LeafNode.java
@@ -1,16 +1,46 @@
-
+/*
+ - LeafNode: Non-Empty Leaf DNANode
+ - Contains exactly one sequence
+*/
 public class LeafNode extends DNANode {
     
-    private char[] sequence;
+    private final char[] sequence;
     
-    public LeafNode(char[] seq)
-    {
-        sequence = seq;
+    // For debugging; get rid of it later
+    public String toString() {return String.valueOf(sequence);}
+    
+    // Creates a Leaf DNANode with the given sequence
+    public LeafNode(char[] sequence) {
+        this.sequence = sequence;
     }
     
-    public void insert(char[] seq)
-    {
-        sequence = seq;
+    public char getCharAt(int level) {
+        if (level >= 0 && level < sequence.length) {
+            return sequence[level];
+        }
+        return 0;
     }
-
+    
+    public boolean containsSequenceOf(LeafNode DNANode) {
+        return String.valueOf(this.sequence).equals(String.valueOf(DNANode.sequence));
+    }
+    
+    public void printInfo(int level, int type) {
+        switch (type) {
+            case DNANode.PRINT_SIMPLE:
+                System.out.println(String.valueOf(sequence));
+                break;
+            case DNANode.PRINT_LENGTHS:
+                System.out.println(String.valueOf(sequence) + " " + sequence.length);
+                break;
+            case DNANode.PRINT_STATS:
+            default:
+                int total = sequence.length;
+                double a = 0, c = 0, t = 0, g = 0;
+                String stats = "A:25.00 C:25.00 G:25.00 T:25.00";
+                // TODO: count A, C, T and G percentages, and create stats string
+                System.out.println(String.valueOf(sequence) + " " + stats);
+                break;
+        }
+    }
 }
