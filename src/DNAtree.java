445e924deec19dfb43e275ed66f84def16b64eb0
tsinghqs <tsingh2324@gmail.com>
2019-03-14 21:53:23 -0400

    main file

--- src/DNAtree.java
+++ src/DNAtree.java
@@ -3,35 +3,118 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 
-/**
- * This is class responsible for running the project properly. 
- * @author tsinghqs
- * @version 2018.02.19
- *
- */
-public class DNAtree 
-{
-    /**
-     * This is the main class and function that runs the project
-     * 
-     * @param args a string value to run the project
-     * @throws FileNotFoundException 
-     */
-    public static void main(String[] args) throws FileNotFoundException
-    {
-        //read file as an argument
-        String fileName = args[0];
-        File file = new File(fileName);
-        Scanner sc = new Scanner(file);
+/*
+- TODO:
+- See if we can do a node.equals() for LeafNode to compare sequence
+- remove()
+- search()
+- main() to read from arg[0] file, and call tree functions
+*/
+
+
+public class DNAtree {
+
+   /**
+    * @param args the command line arguments
+    */
+   public static void main(String[] args) {
+       // TODO code application logic here
+       DNAtree tree = new DNAtree();
+       tree.print();
+       tree.insert("A");
+       tree.printLengths();
+       tree.printStats();
        
-        //initialize BST
-        while (sc.hasNextLine())
-        {
-            String line = sc.nextLine();
-            Parser parse = new Parser(line);
-            parse.parseString();
-        
-        }
-        sc.close();
-    }
+       tree.insert("AA");
+       tree.print();
+       
+       tree.insert("AA");
+       tree.insert("A");
+   }
+   
+   private DNANode root = EmptyNode.getInst();
+   
+   /*
+    - isValidSequence(sequence): private/static method validates the sequence
+    - cannot be empty
+    - must conform to {A, C, G, T} charset.
+   */
+   private static final String DNA_CHARSET = "ACGT";
+   private boolean isValidSequence(char[] sequence) {
+       // Must be non-empty
+       if (sequence.length == 0) {
+           return false;
+       }
+       // Must conform to 
+       for (char c : sequence) {
+           if (DNA_CHARSET.indexOf(c) < 0) {
+               return false;
+           }
+       }
+       return true;
+   }
+   
+   /*
+    - print(): Public method to dump the tree
+    - prints each node on a separate line in pre-order traversal
+   */
+   public void print() {
+       root.print(0, DNANode.PRINT_SIMPLE);
+   }
+   /*
+    - printLengths(): Public method to dump the tree
+    - prints each node on a separate line in pre-order traversal
+    - prints sequences and lengths
+   */
+   public void printLengths() {
+       root.print(0, DNANode.PRINT_LENGTHS);
+   }
+   /*
+    - printStats(): Public method to dump the tree
+    - prints each node on a separate line in pre-order traversal
+    - prints sequences and stats
+   */
+   public void printStats() {
+       root.print(0, DNANode.PRINT_STATS);
+   }
+   
+   /*
+    insert(): Public method which inserts a given sequence
+    - sequence must contain one or mode characters from {A, C, G, T}
+    - sequence cannot already exist in the tree
+    - returns the new ROOT node, if the insert changes the root
+    - returns null if the insert does not change the root
+    - always prints an appropriate message
+   */
+   public void insert(String sequence_str) {
+       
+       char[] sequence = sequence_str.toCharArray();
+       
+       // Sanity check on the given sequence
+       if (! isValidSequence(sequence)) {
+           System.out.println("Error: Invalid sequence!");
+           return;
+       }
+       
+       // Create a new leaf node
+       LeafNode newnode = new LeafNode(sequence);
+       
+       // Insert the newnode starting at root
+       if (root instanceof EmptyNode) {
+           root = newnode;
+       } else if (root instanceof LeafNode) {
+           LeafNode LeafNode = (LeafNode) root;
+           if (LeafNode.containsSequenceOf(newnode)) {
+               System.out.println("Error: The sequence already exists!");
+               return;
+           }
+           root = new InternalNode();
+           InternalNode inode = (InternalNode) root;
+           inode.insert(0, LeafNode);
+           inode.insert(0, newnode);
+       } else {
+           InternalNode inode = (InternalNode) root;
+           inode.insert(0, newnode);
+       }
+   }
 }
